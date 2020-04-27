887cbed38597b30ebfb35c88a2f12a1e0684779c
Amith Raravi <amith.raravi@gmail.com>
2020-04-27 03:59:15 +0200

    Imported LOGIN bit from raravi.react Set up complete!

---
+++ components/login/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { LoginContextProvider } from './context';
+import { LoginPage } from './LoginPage';
+
+/**
+ * Login Component: Can be reused across applications.
+ * It requires 2 props:
+ * 1. loginSuccessCallback: For handling
+ *    the 'response' object upon successful login.
+ * 2. apiDetails: An object containing below details
+ *    {
+ *      url: 'http://www.url.com',
+ *      endpoints: {
+ *        login: '/login',
+ *        register: '/register',
+ *        forgotPassword: '/forgotpassword'
+ *      }
+ *    }
+ */
+const Login = (props) => {
+  return (
+    <LoginContextProvider>
+      <LoginPage
+        loginSuccessCallback={props.loginSuccessCallback}
+        apiDetails={props.apiDetails} />
+    </LoginContextProvider>
+  )
+}
+
+// Set default props
+Login.defaultProps = {
+  loginSuccessCallback: (response) => {console.log("Response: ", response)},
+  apiDetails: {
+    url: 'http://localhost:8000',
+    endpoints: {
+      login: '/api/users/login',
+      register: '/api/users/register',
+      forgotPassword: '/api/users/forgotpassword',
+      resetPassword: '/api/users/resetpassword'
+    }
+  }
+};
+
+export default Login;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+import Login from '@bit/raravi.react.login';
+import './App.css';
 
 function App() {
+  const apiDetails = {
+    url: 'http://localhost:8000',
+    endpoints: {
+      login: '/api/users/login',
+      register: '/api/users/register',
+      forgotPassword: '/api/users/forgotpassword',
+      resetPassword: '/api/users/resetpassword'
+    }
+  };
+
+  function onSuccessfulLogin(response) {
+    console.log("Response: ", response);
+  }
+
   return (
-    <div className="container">
-      <p>Hello World!</p>
-    </div>
+    <Switch>
+      <Route path="/app">
+        <p>App</p>
+      </Route>
+      <Route path="/">
+        <Login
+          loginSuccessCallback={onSuccessfulLogin}
+          apiDetails={apiDetails} />
+      </Route>
+    </Switch>
   );
 }
 
